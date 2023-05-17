5644a9bb0a2094e5608e511cec11d3b570331fef
SamRemix <s_capron@hotmail.fr>
2023-05-17 18:19:57 +0200

    auth api test

---
+++ back_/src/auth/auth.spec.ts
@@ -0,0 +1,46 @@
+import request from 'supertest'
+
+import app from '../app'
+
+const tester = {
+  id: '643d66769fcf9d57b5864eea',
+  name: 'Tester',
+  email: 'tester@test.com',
+  password: 'Tester123!'
+}
+
+describe('signup', () => {
+  afterAll(async () => {
+    await request(app)
+      .delete(`/users/${tester.id}`)
+  })
+
+  it('create a new user', async () => {
+    const { status } = await request(app)
+      .post('/auth/signup')
+      .send(tester)
+
+    expect(status).toBe(200)
+  })
+})
+
+describe('login', () => {
+  beforeAll(async () => {
+    await request(app)
+      .post('/auth/signup')
+      .send(tester)
+  })
+
+  afterAll(async () => {
+    await request(app)
+      .delete(`/users/${tester.id}`)
+  })
+
+  it('return user', async () => {
+    const { status } = await request(app)
+      .post('/auth/login')
+      .send(tester)
+
+    expect(status).toBe(200)
+  })
+})
