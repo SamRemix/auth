af14c873cdc8110de735dd70aa58ddb83d2af6f2
SamRemix <s_capron@hotmail.fr>
2023-05-19 13:12:07 +0200

    refactoring

--- back_/src/components/auth/auth.spec.ts
+++ back_/src/components/auth/auth.spec.ts
@@ -1,11 +1,12 @@
-import prisma from '../../prisma'
 import app from '../../app'
 
 import request from 'supertest'
 
 import UsersService from '../users/users.service'
 
-const usersService = new UsersService()
+import findUserByEmail from '../../utils/findUserByEmail'
+
+const { remove } = new UsersService()
 
 const tester = {
   name: 'Tester',
@@ -14,33 +15,27 @@ const tester = {
 }
 
 afterAll(async () => {
-  const { id } = await prisma.user.findUnique({
-    where: {
-      email: 'tester@test.com'
-    }
-  }) as { id: string }
+  const { id } = await findUserByEmail(tester.email) as { id: string }
 
-  await usersService.remove(id)
+  await remove(id)
 })
 
 describe('POST /auth/signup', () => {
   it('sign up', async () => {
-    const { status, body } = await request(app)
+    const { status } = await request(app)
       .post('/auth/signup')
       .send(tester)
 
     expect(status).toEqual(200)
-    expect(body.user.name).toEqual(tester.name)
   })
 })
 
 describe('POST /auth/login', () => {
   it('log in', async () => {
-    const { status, body } = await request(app)
+    const { status } = await request(app)
       .post('/auth/login')
       .send(tester)
 
     expect(status).toBe(200)
-    expect(body.user.name).toEqual(tester.name)
   })
 })
