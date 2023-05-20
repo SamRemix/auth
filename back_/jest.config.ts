d1767f51c3f369ac1238bd76ea499dc4673f9453
SamRemix <s_capron@hotmail.fr>
2023-05-20 11:18:01 +0200

    specify root directory for tests

--- back_/jest.config.ts
+++ back_/jest.config.ts
@@ -2,7 +2,8 @@ import type { Config } from '@jest/types'
 
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
-  testEnvironment: 'node'
+  testEnvironment: 'node',
+  rootDir: 'src'
 }
 
 export default config
